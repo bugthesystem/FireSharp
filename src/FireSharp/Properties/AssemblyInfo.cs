bda11f04bc8a9f08bea59afd3a3ae44e743258ea
Ziya SARIKAYA <sarikayaziya@gmail.com>
2014-09-06 02:59:26 +0300

    Fix #3

--- src/FireSharp/Properties/AssemblyInfo.cs
+++ src/FireSharp/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
 [assembly: InternalsVisibleTo("FireSharp.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
