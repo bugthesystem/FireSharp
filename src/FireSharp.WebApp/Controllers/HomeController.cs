fa74598bd4d0859cdffbee2486a505f1ff359bd5
Ziya SARIKAYA <sarikayaziya@gmail.com>
2014-12-23 14:31:02 +0200

    Implement simple usage with Autofac to sample project

--- src/FireSharp.WebApp/Controllers/HomeController.cs
+++ src/FireSharp.WebApp/Controllers/HomeController.cs
@@ -1,16 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using FireSharp.Interfaces;
 
 namespace FireSharp.WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IFirebaseClient _firebaseClient;
+
+        public HomeController(IFirebaseClient firebaseClient)
+        {
+            _firebaseClient = firebaseClient;
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult CallFirebase()
+        {
+            _firebaseClient.Push("chat/", new
+            {
+                name = "someone",
+                text = "Hello from backend :" + DateTime.Now.ToString("f")
+            });
+
+            return RedirectToAction("Index");
         }
     }
 }
