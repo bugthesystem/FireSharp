e788de5111456cf060816651e67cab58af028d06
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-01-21 15:21:00 +0200

    Update FireSharp nuspec

--- FireSharp/Properties/AssemblyInfo.cs
+++ FireSharp/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 [assembly: InternalsVisibleTo("FireSharp.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
