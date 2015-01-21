b204c853cb354046578f9b9b70ddbfce971f7a49
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-01-21 14:57:28 +0200

    FireSharp project converted to portable library.

--- FireSharp/Properties/AssemblyInfo.cs
+++ FireSharp/Properties/AssemblyInfo.cs
@@ -1,29 +1,20 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Resources;
+using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("FireSharp")]
-[assembly: AssemblyDescription(".Net wrapper for Firebase backend API")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FireSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("2bdbf3c7-1dfe-4484-b328-35960b586953")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,7 +26,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.1.0")]
 [assembly: AssemblyFileVersion("1.1.0")]
 [assembly: InternalsVisibleTo("FireSharp.Tests")]
