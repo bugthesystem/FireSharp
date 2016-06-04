1f123ddc6ffe01edd661a9cf04f7084a9dc83f18
Clément Bourgeois <moonpyk@gmail.com>
2016-06-04 19:39:50 +0200

    Code cleanup, removing HttpResponseMessage from FirebaseResponse.
    The property was never set inside the library, removed constructors too.

--- FireSharp/Response/PushResponse.cs
+++ FireSharp/Response/PushResponse.cs
@@ -1,15 +1,9 @@
 ﻿using System.Net;
-using System.Net.Http;
 
 namespace FireSharp.Response
 {
     public class PushResponse : FirebaseResponse
     {
-        public PushResponse(string body, HttpStatusCode statusCode, HttpResponseMessage httpResponse)
-            : base(body, statusCode, httpResponse)
-        {
-        }
-
         public PushResponse(string body, HttpStatusCode statusCode)
             : base(body, statusCode)
         {
