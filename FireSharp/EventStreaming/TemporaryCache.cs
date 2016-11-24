eb57fc115f9228f92665ad8897ab32b10be587a7
Zeeshan Ali <makzee81@gmail.com>
2016-11-24 15:44:03 -0500

    Fixed build failure issue. EndOfObject should have been EndObject.

--- FireSharp/EventStreaming/TemporaryCache.cs
+++ FireSharp/EventStreaming/TemporaryCache.cs
@@ -111,7 +111,7 @@ namespace FireSharp.EventStreaming
                     case JsonToken.Null:
                         DeleteChild(root);
                         return;
-                    case JsonToken.EndOfObject: break;
+                    case JsonToken.EndObject: break;
                 }
             }
         }
