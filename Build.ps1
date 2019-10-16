89003c69236c79603b107fcff0a69fed38eaa5c3
Carmel Barak <lablabla@gmail.com>
2019-10-16 19:34:28 +0300

    Updated Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,64 +1,56 @@
-<#
-.SYNOPSIS
-    You can add this to you build script to ensure that psbuild is available before calling
-    Invoke-MSBuild. If psbuild is not available locally it will be downloaded automatically.
-#>
-function EnsurePsbuildInstalled{
-    [cmdletbinding()]
-    param(
-        [string]$psbuildInstallUri = 'https://raw.githubusercontent.com/ligershark/psbuild/master/src/GetPSBuild.ps1'
-    )
-    process{
-        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
-            'Installing psbuild from [{0}]' -f $psbuildInstallUri | Write-Verbose
-            (new-object Net.WebClient).DownloadString($psbuildInstallUri) | iex
-        }
-        else{
-            'psbuild already loaded, skipping download' | Write-Verbose
-        }
+# https://www.appveyor.com/docs/environment-variables/
 
-        # make sure it's loaded and throw if not
-        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
-            throw ('Unable to install/load psbuild from [{0}]' -f $psbuildInstallUri)
-        }
-    }
+# Script static variables
+$buildDir = $env:APPVEYOR_BUILD_FOLDER # e.g. C:\projects\rn-common\
+$buildNumber = $env:APPVEYOR_BUILD_VERSION # e.g. 1.0.17
+
+# C:\projects\rn-common\src\Rn.Common
+$projectDir = $buildDir + "\FireSharp.Core";
+# C:\projects\rn-common\src\Rn.Common\Rn.Common.csproj
+$projectFile = $projectDir + "\FireSharp.Core.csproj";
+# C:\projects\rn-common\src\Rn.CommonTests
+$testDir = $buildDir + "\FireSharp.Tests.Core";
+# C:\projects\rn-common\src\Rn.Common\Rn.Common.x.x.x.nupkg
+$nugetFile = $projectDir + "\FireSharp.Core." + $buildNumber + ".nupkg";
+
+
+# Display .Net Core version
+Write-Host "Checking .NET Core version" -ForegroundColor Green
+& dotnet --version
+
+# Restore the main project
+Write-Host "Restoring project" -ForegroundColor Green
+& dotnet restore $projectFile --verbosity m
+
+# Publish the project
+Write-Host "Publishing project" -ForegroundColor Green
+& dotnet publish $projectFile
+
+# Discover and run tests
+Write-Host "Running tests" -ForegroundColor Green
+cd $testDir
+& dotnet restore FireSharp.Tests.Core.csproj --verbosity m
+$testOutput = & dotnet test | Out-String
+Write-Host $testOutput
+
+# Ensure that the tests passed
+if ($testOutput.Contains("Test Run Successful.") -eq $False) {
+  Write-Host "Build failed!";
+  Exit;
 }
 
-# Taken from psake https://github.com/psake/psake
+# Generate a NuGet package for publishing
+Write-Host "Generating NuGet Package" -ForegroundColor Green
+cd $projectDir
+& dotnet pack -c Release /p:PackageVersion=$buildNumber -o $projectDir
 
-<#
-.SYNOPSIS
-  This is a helper function that runs a scriptblock and checks the PS variable $lastexitcode
-  to see if an error occcured. If an error is detected then an exception is thrown.
-  This function allows you to run command-line programs without having to
-  explicitly check the $lastexitcode variable.
-.EXAMPLE
-  exec { svn info $repository_trunk } "Error executing SVN. Please verify SVN command-line client is installed"
-#>
-function Exec
-{
-    [CmdletBinding()]
-    param(
-        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
-        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
-    )
-    & $cmd
-    if ($lastexitcode -ne 0) {
-        throw ("Exec: " + $errorMessage)
-    }
-}
+# Save generated artifacts
+Write-Host "Saving Artifacts" -ForegroundColor Green
+Push-AppveyorArtifact $nugetFile
 
-if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+# Publish package to NuGet
+Write-Host "Publishing NuGet package" -ForegroundColor Green
+#& nuget push $nugetFile -ApiKey $env:NUGET_API_KEY -Source https://www.nuget.org/api/v2/package
 
-EnsurePsbuildInstalled
-
-exec { & dotnet restore }
-
-Invoke-MSBuild
-
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
-
-exec { & dotnet test .\FireSharp.Tests.Core -c Release }
-
-exec { & dotnet pack .\FireSharp.Core -c Release -o .\artifacts --version-suffix=$revision }
+# Done
+Write-Host "Done!" -ForegroundColor Green
